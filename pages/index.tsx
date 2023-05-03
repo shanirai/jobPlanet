41f1f8bc818c48ae50f5f5c965d1a365d1b345f0
shanirai <shani.rai@mckinleyrice.com>
2023-05-03 11:01:28 +0530

    initial setup

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div
+      className={`flex min-h-screen flex-col items-center justify-between p-24 `}
+    >
+      <h2 className="text-3xl font-bold underline">
+        Hello Shani Welcome on Tailwind CSS
+      </h2>
+    </div>
+  );
+}
